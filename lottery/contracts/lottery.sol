c43d19542f6cf4342fc0b483020ec52330c33bf9
manu <manuel.adalidmoya@jobtome.com>
2023-09-13 19:57:58 +0200

    lottery challenge 4

--- lottery/contracts/lottery.sol
+++ lottery/contracts/lottery.sol
@@ -28,6 +28,7 @@ contract Lottery {
         require(msg.sender == manager || players.length >= 10, "Only the manager can pick a winner if there are less than 10 players");
         require(players.length >= 3, "Need at least 3 players before picking a winner");
 
+        payable(manager).transfer(getBalance()/10);
         players[random() % players.length].transfer(getBalance());
         players = new address payable[](0);
     }
