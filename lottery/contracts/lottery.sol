f9ca7fa3f6a81996937d70aa0b9504de64303bd1
manu <manuel.adalidmoya@jobtome.com>
2023-09-13 19:49:58 +0200

    lottery contract

---
+++ lottery/contracts/lottery.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Lottery {
+    address payable[] public players;
+    address public manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    receive() external payable {
+        require(msg.value == 0.1 ether, "Only accepting 0.1 ether purchases");
+
+        players.push(payable(msg.sender));
+    }
+
+    function getBalance() public view returns(uint) {
+        require(msg.sender == manager, "Only the manager can see the balance");
+
+        return address(this).balance;
+    }
+
+    function pickWinner() public {
+        require(msg.sender == manager, "Only the manager can pick a winner");
+        require(players.length >= 3, "Need at least 3 players before picking a winner");
+
+        players[random() % players.length].transfer(getBalance());
+        players = new address payable[](0);
+    }
+
+    function random() internal view returns(uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    }
+}
